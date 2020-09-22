875d8756e23cdb1b22577daf141407629849759c
madiseniman <madisen.whitfield@gmail.com>
2020-09-22 04:06:54 +0200

    Format Code and fix an image size bug

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 let canvasShowing;
 let imageShowing;
-function show (image){
-    if (canvasShowing){
+
+function show(image) {
+    if (canvasShowing) {
         document.getElementById("canvas").style.display = "none";
         canvasShowing = false;
     }
     document.getElementById("edit-image").style.display = "block";
-    document.getElementById("edit-image").src=image;
+    document.getElementById("edit-image").src = image;
     imageShowing = true;
 }
-function blackWhite(image, edit) {
+
+function edit(image, edit) {
     const img = image;
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
     img.crossOrigin = "anonymous";
-    ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height,);
     for (let i = 0; i < imgData.data.length; i += 4) {
         let count = imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2];
         let colour = 0;
@@ -27,19 +29,19 @@ function blackWhite(image, edit) {
             imgData.data[i + 1] = colour;
             imgData.data[i + 2] = colour;
             imgData.data[i + 3] = 255;
-        } else if (edit === "inv"){
-                imgData.data[i]     = 255 - imgData.data[i];     // red
-                imgData.data[i + 1] = 255 - imgData.data[i + 1]; // green
-                imgData.data[i + 2] = 255 - imgData.data[i + 2]; // blue
+        } else if (edit === "inv") {
+            imgData.data[i] = 255 - imgData.data[i];     // red
+            imgData.data[i + 1] = 255 - imgData.data[i + 1]; // green
+            imgData.data[i + 2] = 255 - imgData.data[i + 2]; // blue
+        }
     }
-    }
-    if (imageShowing){
+    if (imageShowing) {
         document.getElementById("edit-image").style.display = "none";
         imageShowing = false;
     }
     document.getElementById("canvas").style.display = "block";
     ctx.putImageData(imgData, 0, 0);
     canvasShowing = true;
-
-
 }
+
+
